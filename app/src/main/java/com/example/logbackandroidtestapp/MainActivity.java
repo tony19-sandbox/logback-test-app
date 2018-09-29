f0e2635e70e27f496a72d7ccc39ac32b4b70fbdd
Tony Trinh <tony19@gmail.com>
2018-09-29 17:21:03 -0500

    Move log writing to FAB

--- app/src/main/java/com/example/logbackandroidtestapp/MainActivity.java
+++ app/src/main/java/com/example/logbackandroidtestapp/MainActivity.java
@@ -29,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Writing logs", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                writeLogs();
             }
         });
     }
@@ -44,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        writeLogs();
-
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
