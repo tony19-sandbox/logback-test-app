4682f5a06607174e6f489537e3a1a2721e6c48fe
Tony Trinh <tony19@gmail.com>
2019-02-21 23:10:02 -0600

    Demo logback-android in unit test

--- app/src/test/java/com/example/ExampleUnitTest.java
+++ app/src/test/java/com/example/ExampleUnitTest.java
@@ -4,12 +4,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.robolectric.RobolectricTestRunner;
+
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        Logger log = LoggerFactory.getLogger(ExampleUnitTest.class);
+        log.info("hello world");
     }
 }
