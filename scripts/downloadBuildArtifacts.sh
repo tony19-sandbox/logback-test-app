8f58fd7fe4e8c838710454644ba6d6ae9ecea180
Tony Trinh <tony19@gmail.com>
2020-01-10 03:37:20 -0600

    build: Add script to download build artifacts

---
+++ scripts/downloadBuildArtifacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+build_number=${CIRCLE_BUILD_NUM}
+username=tony19
+project=logback-android
+
+artifactUrl=$(curl https://circleci.com/api/v1.1/project/github/$username/$project/$build_number/artifacts?circle-token=$CIRCLE_TOKEN | jq '.[] | select(.path=="outputs/aar/logback-android-debug.aar") | .url')
+
+temp="${artifactUrl%\"}"
+artifactUrl="${temp#\"}"
+
+mkdir -p app/libs
+curl $artifactUrl --output app/libs/logback-android.aar
