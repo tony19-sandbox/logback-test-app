d44c2d941f084f264243b1a0933275148707cb81
Tony Trinh <tony19@gmail.com>
2020-05-21 02:10:55 -0500

    update unit test

--- app/src/test/java/com/example/ExampleUnitTest.java
+++ app/src/test/java/com/example/ExampleUnitTest.java
@@ -17,9 +17,23 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void log() throws Exception {
         assertEquals(4, 2 + 2);
+
         Logger log = LoggerFactory.getLogger(ExampleUnitTest.class);
-        log.info("hello world");
+        log.info("info message");
+        log.info("info message", new RuntimeException("dummy error"));
+        log.debug("debug message");
+        log.debug("debug message", new RuntimeException("dummy error"));
+        log.trace("trace message");
+        log.trace("trace message", new RuntimeException("dummy error"));
+        log.warn("warn message");
+        log.warn("warn message", new RuntimeException("dummy error"));
+        log.error("error message");
+        log.error("error message", new RuntimeException("dummy error"));
+
+//        LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+//        boolean hasError = loggerContext.getStatusManager().getCopyOfStatusList().stream().anyMatch(s -> s instanceof ErrorStatus);
+//        assertFalse("no errors occurred in config", hasError);
     }
 }
