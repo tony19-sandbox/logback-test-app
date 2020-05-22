c00404d2a639a671df92e92fef2dd4089c3f89a4
Tony Trinh <tony19@gmail.com>
2020-05-22 02:00:05 -0500

    check sdk version

--- app/src/androidTest/java/com/example/ApplicationTest.java
+++ app/src/androidTest/java/com/example/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,6 +12,11 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
+    @Test
+    public void checkSdkVersion() {
+        assertEquals(23, Build.VERSION.SDK_INT);
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
